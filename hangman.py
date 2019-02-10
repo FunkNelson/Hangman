17d122a73bbba64a2584612f01f77027915eb241
FunkNelson <rossmb@gmail.com>
2019-02-10 11:06:54 -0800

    added hangman

--- hangman.py
+++ hangman.py
@@ -23,6 +23,7 @@ def isWordSolved(word):
 		if i == '_':
 			return False
 	# if no masked letters are found
+	print 'Congratulations, the word has been solved!'
 	return True
 			
 
@@ -31,6 +32,26 @@ def userGuess(guessedLetters):
 	guess = raw_input("Guess a letter: ")
 	#print "Your guess is: ", guess
 	guessedLetters.append(guess)
+
+
+def printHangman():
+	line1 = '  __________'
+	line2 = '  //      |'
+	line3 = ' //       O'
+	line4 = '/||      /|\\ '
+	line5 = ' ||      / \\'
+	line6 = ' ||'
+	line7 = '----'
+	line8 = ''
+
+	print line1
+	print line2
+	print line3
+	print line4
+	print line5
+	print line6
+	print line7
+	print line8
 
 # GLOBAL
 guessedLetters = []
@@ -45,4 +66,8 @@ print maskWord(word, guessedLetters)
 
 while (isWordSolved(maskWord(word, guessedLetters)) == False):
 	userGuess(guessedLetters)
+	print ''
 	print maskWord(word, guessedLetters)
+	print ''
+	printHangman()
+
